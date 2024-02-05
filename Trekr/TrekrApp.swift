ded4b0b9edc6d68b99fe7d11e18d93b450480bae
Ben Michael <ben29michael@gmail.com>
2024-02-05 15:47:08 -0800

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Benjamin Michael on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
